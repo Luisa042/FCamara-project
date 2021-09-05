d209cf4023857dbfdb653360f340c4f3696c149e
Thiarhes <injesusservice@hotmail.com>
2021-09-05 14:35:19 -0300

    metodo register criado no repository

--- repository/user.dao.js
+++ repository/user.dao.js
@@ -6,57 +6,52 @@ class UserRepository {
         this.user = UserModel;
     }
 
-    register = async (user) {
-        // payload
-        const { name, email, password } = user;
+    register = async (user) { // payload
+        const {name, email, password} = user;
 
         try {
-            const existsUser = await this.user.findOne({ name });
-            const existsEmail = await this.user.findOne({ email });
+            const existsUser = await this.user.findOne({name});
+            const existsEmail = await this.user.findOne({email});
             const validPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,99}$/;
-            const validEmail = /^[a-z0-9.]+@fcamara.com.br$/; 
+            const validEmail = /^[a-z0-9.]+@fcamara.com.br$/;
 
             // validando email da fcamara
-        if (!email.match(emailValidate)) {
-        return res.status(400).json({ msg: 'email não permitido, tente novamente usando @fcamara.com.br' });
-        }
+            if (!email.match(validEmail)) {
+                throw new Error('Email não permitido, tente novamente usando @fcamara.com.br');
+            }
 
-        // validando senha segura
-        if (!password.match(passValidate)) {
-        return res.status(400).json({ msg: 'Por motivos de segurança, sua senha deve conter pelo menos 6 caracteres, 1 letra maiúscula, 1 símbolo e 1 número'});
-        }
+            // validando senha segura
+            if (!password.match(validPass)) {
+                throw new Error('Por motivos de segurança, sua senha deve conter pelo menos 6 caracteres, 1 letra maiúscula, 1 símbolo e 1 número');
+            }
 
-        // verificando se usuário já está registrado
-        if (existsUser) {
-        return res.status(400).json({ msg: 'Este usuário já está registrado' });
-        }
+            // verificando se usuário já está registrado
+            if (existsUser) {
+                throw new Error('Este usuário já está registrado');
+            }
 
-        // verificando se o email já está registrado
-        if (existsEmail) {
-        return res.status(400).json({ msg: 'Este email já está registrado' });
+            // verificando se o email já está registrado
+            if (existsEmail) {
+                throw new Error('Este email já está registrado');
 
-        // verificando se todos os campos estão preenchidos
-        } else if (!name || !email || !password) {
-        return res.status(400).json({ msg: 'Todos os campos são obrigatórios'});
+            // verificando se todos os campos estão preenchidos
+            } else if (!name || !email || !password) {
+                throw new Error('Todos os campos são obrigatórios');
 
-        // criando novo usuário
-        } else {
-            const salt = await bcrypt.genSalt(10); 
-            password = await bcrypt.hash(password, salt);
-            let newUser = await User.create({ 
-                name, 
-                email, 
-                password 
-            });
-        return res.status(200).json({
-            name: newUser.name,
-            email: newUser.email,
-            id: newUser.id
-        });
-    }
-            
+            // criando novo usuário
+            } else {
+                const salt = await bcrypt.genSalt(10);
+                password = await bcrypt.hash(password, salt);
+                let newUser = await this.user.create({name, email, password});
+                return ({
+                    name: newUser.name,
+                    email: newWuser.email,
+                    id: newUser.id
+                });
+            }
+
         } catch (error) {
-            
+            throw new Error(error);
         }
     }
 }
