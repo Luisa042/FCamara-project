5f6369e3ac20f767bf665711a89094f15ac5e9c6
Thiarhes <injesusservice@hotmail.com>
2021-09-05 23:38:19 -0300

    feat(*): rota de login adicionada e testada

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const userRepo = require('../repository/user.dao');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = Router();
 
 router.post('/register', async (req, res) => {
@@ -9,6 +11,43 @@ router.post('/register', async (req, res) => {
         
     } catch (error) {
         return res.status(500).json(error.message);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userRepo.findUser(email);
+        
+        if (!user) {
+            return res.status(400).json();
+        }
+        
+        const compareHash = bcrypt.compareSync(password, user.password);
+
+        
+        if (!compareHash) {
+            return res.status(400).json();
+        }
+        
+        const payload = {
+            id: user.id,
+            user: user.name,
+            email: user.email
+        };
+        
+        const token = jwt.sign(
+            payload,
+            process.env.SECRET_JWT,
+            {
+                expiresIn: process.env.EXPIRATION_TOKEN
+            },
+            );
+            
+            res.status(200).json({ payload, token });
+            
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
