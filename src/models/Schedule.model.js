00a70475ca96af9e90527c2dab429bdc67f78ba9
Thiarhes <injesusservice@hotmail.com>
2021-09-07 22:13:02 -0300

    feat(*): model schedule finalizado

--- src/models/Schedule.model.js
+++ src/models/Schedule.model.js
@@ -1,11 +1,23 @@
 const { Schema, model } = require('mongoose');
 
 const scheduleSchema = new Schema({
-    
+    localId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Local',
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        required: true
+    }
 },
-{
-    timestamps: true
-}
+    {
+        timestamps: true
+    }
 );
 
 module.exports = model('Schedule', scheduleSchema);
