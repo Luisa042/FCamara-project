4fe61166bbec2cf1b9751836084ecfdb364860b6
Thiarhes <injesusservice@hotmail.com>
2021-09-08 21:37:08 -0300

    feat(schedule.dao): schedule.dao finalizado

--- src/repository/schedule.dao.js
+++ src/repository/schedule.dao.js
@@ -5,7 +5,55 @@ class ScheduleRepository {
         this.schedule = ScheduleModel;
     }
 
-    
+    listAllAppointments = async () => {
+        try {
+            const schedules = await this.schedule.find();
+            return schedules;
+        } catch (error) {
+            throw new Error();
+        }
+    };
+
+    listOneAppointment = async (id) => {
+        try {
+            const schedule = await this.schedule.findById(id);
+            return schedule;
+        } catch (error) {
+            throw new Error();
+        }
+    }
+
+    makeAppointment = async (schedule) => {
+        const { city, userId, date } = schedule;
+
+        try {
+                const appointmentDate = new Date(date);
+
+                const newAppointment = await this.schedule.create({
+                    city,
+                    user: userId,
+                    date: appointmentDate
+                });
+                return ({
+                    city: newAppointment.city,
+                    user: newAppointment.user,
+                    date: newAppointment.date,
+                    id: newAppointment.id
+                });
+                
+            } catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    cancelAppointment = async (id) => {
+        try {
+            const delAppointment = await this.schedule.findByIdAndDelete(id);
+            return delAppointment;
+        } catch (error) {
+            throw new Error();
+        }
+    }
         
 };
 
